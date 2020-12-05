04ada31fe3f10eca8357484673467dc3f392d672
Abuzzz007 <abilsavio@gmail.com>
2020-12-06 04:49:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Demo react package
+# Installation
+```
+npm install 
+```
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div id="home">
+            <h1>This site is still under development😕</h1>
+        </div>
+    )
+}
+
+export default Home;
